3498e2c9a52afc9b978e02d5983d21d2ceade6f6
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-18 15:02:33 +0200

    all "mangaList" associative array elements info printed in homepage with foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     <h1>Laravel Primi Passi</h1>
+    <h2>Hello World</h2>
+
+    @foreach ($mangaList as $manga)
+        <div>
+            <h3>{{ $manga['title'] }}</h3>
+            <h4>{{ $manga['author'] }}</h4>
+            <p>{{ $manga['plot'] }}</p>
+        </div>
+    @endforeach;
 </body>
 </html>
