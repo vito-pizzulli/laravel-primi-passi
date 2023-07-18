80af25ee9ae847ee1c8b2ded274b6843652bda16
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-18 14:20:58 +0200

    deleted welcome.blade.php, init home.blade.php, added that the route displays home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+<body>
+    <h1>Laravel Primi Passi</h1>
+</body>
+</html>
